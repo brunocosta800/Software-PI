3d6f4d94bc8a1d9e66b498e168d71c87394d2e27
Bruno Costa Dourado <134982659+brunocosta800@users.noreply.github.com>
2024-11-06 11:14:35 -0300

    Add files via upload

---
+++ Software_PI/about.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def app():
+    st.markdown('<h1 style="text-align: center;">O que é o <span style="color: #408CFF;">SmartTravel Insights</span>?</h1>', unsafe_allow_html=True)
+    st.text('')
