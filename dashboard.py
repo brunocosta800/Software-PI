6b678fccdcbc923c8e7631fe0eb886ea7729887c
brunocosta <brunoc.dourado10@gmail.com>
2024-11-14 18:17:02 -0300

    Implementação de gráficos

--- dashboard.py
+++ dashboard.py
@@ -3,21 +3,24 @@ from dash import dcc, html
 import requests
 import pandas as pd
 
-urlDestinosNormal = "http://192.168.58.95:5000/destinosNormais"
+urlDestinosNormal = "http://192.168.15.43:5000/destinosNormais"
 response = requests.get(urlDestinosNormal)
 dadosDestinosNormal = response.json()
 Top5CidadesNormal = pd.DataFrame(dadosDestinosNormal)
 
-urlDestinosFeriado = "http://192.168.58.95:5000/destinosFeriado"
+urlDestinosFeriado = "http://192.168.15.43:5000/destinosFeriado"
 response = requests.get(urlDestinosFeriado)
 dadosDestinosFeriado = response.json()
 Top5CidadesFeriado = pd.DataFrame(dadosDestinosFeriado)
 
+urlVoosPorFeriado = "http://192.168.15.43:5000/voosPorFeriado"
+responseVoosPorFeriado = requests.get(urlVoosPorFeriado)
+dadosVoosPorFeriado = responseVoosPorFeriado.json()
+voosPorFeriado = pd.DataFrame(dadosVoosPorFeriado)
+
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
-    html.H1("Top Cidades Mais Visitadas", style={'textAlign': 'center', 'fontFamily': 'Arial', 'fontSize': '36px', 'color': 'darkblue'}),
-
     html.Div([
         html.H2("Top 5 Cidades Mais Visitadas em Dias Normais", style={'textAlign': 'center', 'fontFamily': 'Arial', 'fontSize': '28px', 'color': 'darkgreen'}),
         dcc.Graph(id='graph-destinos-normais', figure={
@@ -36,7 +39,7 @@ app.layout = html.Div([
                     'showgrid': False,  
                     'tickangle': 0,  
                     'tickmode': 'array',  
-                    'tickvals': Top5CidadesFeriado['cidade'],  
+                    'tickvals': Top5CidadesNormal['cidade'],  
                     'tickfont': {'size': 10, 'family': 'Arial'},  
                 },
                 'title': 'Destinos Mais Visitados em Dias Normais',
@@ -67,6 +70,33 @@ app.layout = html.Div([
                     'tickfont': {'size': 10, 'family': 'Arial'},  
                 },
                 'title': 'Destinos Mais Visitados em Feriados',
+            }
+        })
+    ], style={'padding': '20px'}),
+    
+    html.Div([
+        html.H2("Quantidade de Vôos Por Feriado", style={'textAlign': 'center', 'fontFamily': 'Arial', 'fontSize': '28px', 'color': 'darkgreen'}),
+        dcc.Graph(id='graph-voos-por-feriado', figure={
+            'data': [{
+                'x': voosPorFeriado['nome feriado'],
+                'y': voosPorFeriado['contagem'],
+                'type': 'bar',
+                'name': 'Feriados'
+            }],
+            'layout': {
+                'bargap': 0.5,
+                'plot_bgcolor': 'rgba(0,0,0,0)',
+                'paper_bgcolor': 'rgba(0,0,0,0)',
+                'xaxis': {
+                    'title': 'Cidades',
+                    'showgrid': False,  
+                    'tickangle': 0,  
+                    'tickmode': 'array',  
+                    'tickvals': voosPorFeriado['nome feriado'],  
+                    'tickfont': {'size': 10, 'family': 'Arial'},  
+                },
+                'title': 'Número de Vôos por feriado',
+                'title_x': 0.5,
             }
         })
     ], style={'padding': '20px'})  
