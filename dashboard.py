8e5f8e0d2d960e7ccdc39ee2d811dbe9935b1dbc
brunocosta <brunoc.dourado10@gmail.com>
2024-11-17 22:09:43 -0300

    Filtros Aplicados

--- dashboard.py
+++ dashboard.py
@@ -7,23 +7,23 @@ import plotly.express as px
 
 app = dash.Dash(__name__)
 
-st.session_state.urlDestinosNormal = f"http://192.168.15.43:5000/destinosNormais?ano={'Todos'}"
+st.session_state.urlDestinosNormal = f"http://192.168.15.43:5000/destinosNormais?ano={'Todos'}&estado={'Todos'}&aero={'Todos'}"
 responseDestinosNormal = requests.get(st.session_state.urlDestinosNormal)
 dadosDestinosNormal = responseDestinosNormal.json()
 Top5CidadesNormal = pd.DataFrame(dadosDestinosNormal)
 
-urlDestinosFeriado = f"http://192.168.15.43:5000/destinosFeriado?ano={'Todos'}"
+urlDestinosFeriado = f"http://192.168.15.43:5000/destinosFeriado?ano={'Todos'}&estado={'Todos'}&aero={'Todos'}"
 responseDestinosFeriado = requests.get(urlDestinosFeriado)
 dadosDestinosFeriado = responseDestinosFeriado.json()
 Top5CidadesFeriado = pd.DataFrame(dadosDestinosFeriado)
 
-urlVoosPorFeriado = f"http://192.168.15.43:5000/voosPorFeriado?ano={'Todos'}"
+urlVoosPorFeriado = f"http://192.168.15.43:5000/voosPorFeriado?ano={'Todos'}&estado={'Todos'}&aero={'Todos'}"
 responseVoosPorFeriado = requests.get(urlVoosPorFeriado)
 dadosVoosPorFeriado = responseVoosPorFeriado.json()
 voosPorFeriado = pd.DataFrame(dadosVoosPorFeriado)
 
-urlVoosPorAno = f"http://192.168.15.43:5000/voosPorAno"
-responseVoosPorAno = requests.get(urlVoosPorAno)
+st.session_state.urlVoosPorAno = f"http://192.168.15.43:5000/voosPorAno?estado={'Todos'}&aero={'Todos'}"
+responseVoosPorAno = requests.get(st.session_state.urlVoosPorAno)
 dadosVoosPorAno = responseVoosPorAno.json()
 voosPorAno = pd.DataFrame(dadosVoosPorAno)
 
@@ -32,34 +32,50 @@ responseFeriadosPorAno = requests.get(urlFeriadosPorAno)
 dadosFeriadosPorAno = responseFeriadosPorAno.json()
 feriadosPorAno = pd.DataFrame(dadosFeriadosPorAno)
 
-st.session_state.urlVoosPorMes = f"http://192.168.15.43:5000/voosPorMes?ano={'Todos'}"
+st.session_state.urlVoosPorMes = f"http://192.168.15.43:5000/voosPorMes?ano={'Todos'}&estado={'Todos'}&aero={'Todos'}"
 responseVoosPorMes = requests.get(st.session_state.urlVoosPorMes)
 dadosVoosPorMes = responseVoosPorMes.json()
 voosPorMes = pd.DataFrame(dadosVoosPorMes)
 
 ano = ["Todos", "2014","2015","2016","2017","2018", "2019","2020","2021","2022","2023"]
+
 estados = 'http://192.168.15.43:5000/nomesEstados'
 responseEstado = requests.get(estados)
 estado = ['Todos'] + responseEstado.json()
+
+aeroportos = ['Todos', 'Congonhas', 'Guarulhos']
 #region graficos
 
 #region dropdowns
 app.layout = html.Div([
-    dcc.Dropdown(
-            id='ano-dropdown',
-            options=[{'label': ano, 'value': ano} for ano in ano],
-            value='Todos' 
-    ),
-    dcc.Dropdown(
-            id='modal-dropdown',
-            options=[{'label': estado, 'value': estado} for estado in estado],
-            value='Todos' 
-    ),
-    dcc.Dropdown(
-            id='destino-dropdown',
-            options=[{'label': 'regiao', 'value': 'regiao'}],
-            value='Todos' 
-    ),
+    html.Div([
+        html.Div([
+            dcc.Dropdown(
+                id='ano-dropdown',
+                options=[{'label': ano, 'value': ano} for ano in ano],
+                value='Todos',
+                style={'width': '100%'}
+            ),
+        ], style={'flex': 1, 'padding': '10px', 'text-align': 'center', 'display': 'flex', 'align-items': 'center'}),
+        
+        html.Div([
+            dcc.Dropdown(
+                id='estado-dropdown',
+                options=[{'label': estado, 'value': estado} for estado in estado],
+                value='Todos',
+                style={'width': '100%'}
+            ),
+        ], style={'flex': 1, 'padding': '10px', 'text-align': 'center', 'display': 'flex', 'align-items': 'center'}),
+        
+        html.Div([
+            dcc.Dropdown(
+                id='aero-dropdown',
+                options=[{'label': aeroportos, 'value': aeroportos} for aeroportos in aeroportos],
+                value='Todos',
+                style={'width': '100%'}
+            ),
+        ], style={'flex': 1, 'padding': '10px'}),
+    ], style={'flex': 1, 'padding': '10px', 'text-align': 'center', 'display': 'flex', 'align-items': 'center'}),
 #endregion
 
 #region graficoTop5Normais
@@ -258,66 +274,90 @@ app.layout = html.Div([
 
 #region cbGraficos
 
-#region cbTop5Normais
+#region cbTop5Feriado
 @app.callback(
     Output('graph-destinos-feriado', 'figure'),
-    Input('ano-dropdown', 'value')
+    Input('ano-dropdown', 'value'),
+    Input('estado-dropdown', 'value'),
+    Input('aero-dropdown', 'value')
 )
-def update_graph_destinos_normais(selected_year):
-    urlDestinosFeriado = f"http://192.168.15.43:5000/destinosFeriado?ano={selected_year}"
+def update_graph_destinos_normais(selected_year, selected_state, selected_aero):
+    urlDestinosFeriado = f"http://192.168.15.43:5000/destinosFeriado?ano={selected_year}&estado={selected_state}&aero={selected_aero}"
     responseDestinosFeriado = requests.get(urlDestinosFeriado)
     dadosDestinosFeriado = responseDestinosFeriado.json()
     Top5CidadesFeriado = pd.DataFrame(dadosDestinosFeriado)
     
-    fig = px.bar(Top5CidadesFeriado, x='cidade', y='contagem')
+    fig = px.bar(Top5CidadesFeriado, x='cidade', y='contagem', color_discrete_sequence=['#2484BF'])
     return fig
 #endregion
 
-#region cbTop5Feriados
+#region cbTop5Normais
 @app.callback(
     Output('graph-destinos-normais', 'figure'),
-    Input('ano-dropdown', 'value')
+    Input('ano-dropdown', 'value'),
+    Input('estado-dropdown', 'value'),
+    Input('aero-dropdown', 'value')
 )
-def update_graph_destinos_feriado(selected_year):
-    urlDestinosNormal = f"http://192.168.15.43:5000/destinosNormais?ano={selected_year}"
+def update_graph_destinos_feriado(selected_year, selected_state, selected_aero):
+    urlDestinosNormal = f"http://192.168.15.43:5000/destinosNormais?ano={selected_year}&estado={selected_state}&aero={selected_aero}"
     responseDestinosNormal = requests.get(urlDestinosNormal)
     dadosDestinosNormal = responseDestinosNormal.json()
     Top5CidadesNormal = pd.DataFrame(dadosDestinosNormal)
     
-    fig = px.bar(Top5CidadesNormal, x='cidade', y='contagem')
+    fig = px.bar(Top5CidadesNormal, x='cidade', y='contagem', color_discrete_sequence=['#2484BF'])
     return fig
 #endregion
 
 #region cbVoosPorFeriado
 @app.callback(
     Output('graph-voos-por-feriado', 'figure'),
-    Input('ano-dropdown', 'value')
+    Input('ano-dropdown', 'value'),
+    Input('estado-dropdown', 'value'),
+    Input('aero-dropdown', 'value')
 )
-def update_graph_voos_por_feriado(selected_year):
-    urlVoosPorFeriado = f"http://192.168.15.43:5000/voosPorFeriado?ano={selected_year}"
+def update_graph_voos_por_feriado(selected_year, selected_state, selected_aero):
+    urlVoosPorFeriado = f"http://192.168.15.43:5000/voosPorFeriado?ano={selected_year}&estado={selected_state}&aero={selected_aero}"
     responseVoosPorFeriado = requests.get(urlVoosPorFeriado)
     dadosVoosPorFeriado = responseVoosPorFeriado.json()
     voosPorFeriado = pd.DataFrame(dadosVoosPorFeriado)
     
-    fig = px.bar(voosPorFeriado, x='nome feriado', y='contagem')
+    fig = px.bar(voosPorFeriado, x='nome feriado', y='contagem', color_discrete_sequence=['#2484BF'])
+    return fig
+#endregion
+
+#region cbVoosPorAno
+@app.callback(
+    Output('graph-voos-por-ano', 'figure'),
+    Input('estado-dropdown', 'value'),
+    Input('aero-dropdown', 'value')
+)
+def update_graph_voos_por_ano(selected_state, selected_aero):
+    urlVoosPorAno = f"http://192.168.15.43:5000/voosPorAno?estado={selected_state}&aero={selected_aero}"
+    responseVoosPorAno = requests.get(urlVoosPorAno)
+    dadosVoosPorAno = responseVoosPorAno.json()
+    voosPorAno = pd.DataFrame(dadosVoosPorAno)
+    
+    fig = px.line(voosPorAno, x='ano', y='contagem', markers=True)
+    fig.update_traces(line=dict(color='#2484BF'), marker=dict(color='#2484BF'))
     return fig
 #endregion
 
 #region cbVoosPorMes
 @app.callback(
     Output('graph-voos-por-mes', 'figure'),
-    Input('ano-dropdown', 'value')
+    Input('ano-dropdown', 'value'),
+    Input('estado-dropdown', 'value'),
+    Input('aero-dropdown', 'value')
 )
-def update_graph_voos_por_mes(selected_year):
-    urlVoosPorMes = f"http://192.168.15.43:5000/voosPorMes?ano={selected_year}"
+def update_graph_voos_por_mes(selected_year, selected_state, selected_aero):
+    urlVoosPorMes = f"http://192.168.15.43:5000/voosPorMes?ano={selected_year}&estado={selected_state}&aero={selected_aero}"
     responseVoosPorMes = requests.get(urlVoosPorMes)
     dadosVoosPorMes = responseVoosPorMes.json()
     voosPorMes = pd.DataFrame(dadosVoosPorMes)
     
-    fig = px.bar(voosPorMes, x='mes', y='contagem')
+    fig = px.bar(voosPorMes, x='mes', y='contagem', color_discrete_sequence=['#2484BF'])
     return fig
 #endregion
-
 
 
 #endregion
